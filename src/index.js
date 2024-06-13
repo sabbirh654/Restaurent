cc61ce6f5ebe96e32587eee52ecfaa01e15315eb
Sabbir Hasan <sabbirh1000@gmail.com>
2024-06-14 01:14:10 +0600

    Style nav button and add button functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+import './style.css'
+
 const content = document.querySelector('#content');
 
 const homeButtonClickEventHandler = function(e) {
     content.innerHTML = "";
     const header = document.createElement('h1');
-    header.textContent = 'Hello from Home ';
+    header.textContent = 'Hello from Home!';
     content.appendChild(header);
 }
 
 const menuButtonClickEventHandler = function(e) {
     content.innerHTML = "";
     const header = document.createElement('h1');
-    header.textContent = 'Hello from Menu';
+    header.textContent = 'Hello from Menu!';
+    content.appendChild(header);
+}
+
+const aboutButtonClickEventHandler = function(e) {
+    content.innerHTML = "";
+    const header = document.createElement('h1');
+    header.textContent = 'Hello from About!';
     content.appendChild(header);
 }
 
@@ -19,3 +28,6 @@ homeButton.addEventListener('click', homeButtonClickEventHandler);
 
 const menuButton = document.querySelector('.menuButton');
 menuButton.addEventListener('click', menuButtonClickEventHandler);
+
+const aboutButton = document.querySelector('.aboutButton');
+aboutButton.addEventListener('click', aboutButtonClickEventHandler);
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-      },
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
@@ -17,4 +17,16 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
 };
